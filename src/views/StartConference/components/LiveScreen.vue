60ca2a075ceffcaef7a9c429952e6aa513025279
fyyyyyyy <877385364@qq.com>
2025-03-27 22:41:52 +0800

    fix(LiveScreen): 修改为用webRTC进行通信

--- src/views/StartConference/components/LiveScreen.vue
+++ src/views/StartConference/components/LiveScreen.vue
@@ -1,163 +1,239 @@
 <script setup>
-import { useSocketStore } from "@/stores/socketStore";
-import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { ref, onMounted } from 'vue'
+import { createPeerConnection, createVideoEle, getLocalMediaStream, getLocalScreenMediaStream, setLocalVideoStream, setRemoteVideoStream } from "@/utils/common.js";
+import io from 'socket.io-client';
 
-const route = useRoute();
-const videoRef = ref(null);
-let stream;
-let mediaRecorder;
-const socketStore = useSocketStore();
-const StartRecording = async () => {
-  try {
-    // 捕获屏幕流
-    const screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-    // 捕获音频流
-    const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+// 使用 ref 引用 DOM 元素
+const roomInput = ref(null)
+const userInput = ref(null);
+const startBtn = ref(null);
+const stopBtn = ref(null);
+const videoBtn = ref(null);
+const screenBtn = ref(null);
+const offerVideo = ref(null);
+let peerMap = new Map();
+let roomUserIdList;
+let connectingUserIdList = [];
+let isStopAudio = false;
+let isStopVideo = false;
 
-    // 合并屏幕和音频流
-    const combinedStream = new MediaStream([...screenStream.getVideoTracks(), ...audioStream.getAudioTracks()]);
 
-    // 将合并后的流设置为 video 元素的源（可选）
-    videoRef.value.srcObject = combinedStream;
+onMounted(async () => {
+  let localStream = await getLocalMediaStream({ video: true, audio: true });
+  setLocalVideoStream(offerVideo.value, localStream);
+  startBtn.value.addEventListener('click', async () => {
+    let roomId = roomInput.value.value;
+    let userId = userInput.value.value;
 
-    // 创建 MediaRecorder
-    const mediaRecorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm' });
-
-    await socketStore.connectSocket();
-    // 创建 WebSocket 连接
-    console.log(socketStore.socket);
-
-    socketStore.socket.onopen = () => {
-      console.log('WebSocket connection established');
-      mediaRecorder.start(1000); // 每1秒发送一次数据
-    };
-
-    socketStore.socket.onerror = (error) => {
-      console.error('WebSocket error:', error);
-      socketStore.socket.close();
-    };
-
-    socketStore.socket.onclose = () => {
-      stream = null;
-      socket = null;
-
-    };
-
-    socketStore.socket.onmessage = async (event) => {
-      console.log(event);
-
-      try {
-        const message = JSON.parse(event.data); // 解析 JSON
-        console.log(message);
-        const base64String = message.data; // 获取 Base64 编码的数据
-        const byteArray = Uint8Array.from(atob(base64String), c => c.charCodeAt(0)); // Base64 解码
-        const arrayBuffer = byteArray.buffer; // 转换成 ArrayBuffer
-
-        console.log("收到的用户 ID：", message.userId);
-        console.log("收到的昵称：", message.nickName);
-        console.log("收到的二进制数据：", arrayBuffer);
-
-      } catch (error) {
-        console.error("WebSocket 消息解析失败:", error);
+    let isInit = false;
+    const serverUrl = "wss://192.168.1.122:3333";
+    const options = {
+      reconnectDelayMat: 10000,
+      transports: ["websocket"],
+      query: {
+        roomId,
+        userId
       }
-    };
-
-
-    // 当有数据可用时发送到后端
-    mediaRecorder.ondataavailable = async (event) => {
-
-      if (event.data.size > 0) {
-        const arrayBuffer = await event.data.arrayBuffer();
-        const chunkSize = 1024 * 1024; // 比如4KB
-        const totalChunks = Math.ceil(arrayBuffer.byteLength / chunkSize);
-        console.log(arrayBuffer.byteLength);
-
-        for (let i = 0; i < arrayBuffer.byteLength; i += chunkSize) {
-          const chunk = arrayBuffer.slice(i, i + chunkSize);
-          const packet = {
-            seq: i + 1,
-            total: totalChunks,
-            data: chunk
-          };
-          socketStore.socket.send(chunk);
+    }
+    let client = new io(serverUrl, options);
+    client.on("connect", () => {
+      console.log("Connection successful!");
+    })
+    client.on("disconnect", () => {
+      console.log("Connection disconnect!");
+    })
+    client.on("error", () => {
+      console.log("Connection error!");
+    })
+    client.on('room-msg', (data) => {
+      console.log(data);
+    })
+    client.on('user-id-list-msg', async (userIdList) => {
+      roomUserIdList = userIdList;
+      connectingUserIdList = roomUserIdList.filter(item => item !== userId);
+      // console.log(connectingUserIdList);
+      for (let id of connectingUserIdList) {
+        let peer = peerMap.get(id);
+        if (!peer) {
+          peer = createPeerConnection();
+          peer.createDataChannel("data-channel")
+          peerMap.set(id, peer)
+          /**
+           * @param {RTCPeerConnectionIceEvent} event
+           */
+          peer.onicecandidate = (event) => {
+            // console.log("candidate");
+            if (event.candidate) {
+              client.emit("candidate-msg", {
+                fromUserId: userId,
+                toUserId: id,
+                candidate: event.candidate
+              })
+            }
+          }
+          /**
+           * @param {RTCTrackEvent} event
+           */
+          peer.ontrack = (event) => {
+            let videoEle = createVideoEle(id);
+            setRemoteVideoStream(videoEle, event.track)
+          }
+          peer.ondatachannel = (e) => {
+            console.log("DataChannel is created");
+            // console.log("ondatachannel", e);
+          }
+          localStream.getTracks().forEach(track => {
+            peer.addTrack(track, localStream)
+          });
+          if (!isInit) {
+            let offerSDP = await peer.createOffer()
+            await peer.setLocalDescription(offerSDP)
+            client.emit("offer-sdp-msg", {
+              fromUserId: userId,
+              toUserId: id,
+              sdp: offerSDP
+            });
+          }
         }
       }
-    };
+      isInit = true
+    })
+    client.on('offer-sdp-msg', async (data) => {
+      let { fromUserId, toUserId, sdp } = data
+      if (userId === toUserId) {
+        console.log(connectingUserIdList, fromUserId, "offer");
+        /**
+         * @type {RTCPeerConnection}
+         */
+        let peer = peerMap.get(fromUserId);
+        if (peer) {
+          await peer.setRemoteDescription(sdp)
+          let answerSDP = await peer.createAnswer();
+          await peer.setLocalDescription(answerSDP)
+          client.emit("answer-sdp-msg", {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+            sdp: answerSDP
+          })
+        }
+      }
+    })
+    client.on('answer-sdp-msg', async (data) => {
+      let { fromUserId, toUserId, sdp } = data;
+      if (userId === toUserId) {
+        console.log(connectingUserIdList, fromUserId, "answer");
+        /**
+        * @type {RTCPeerConnection}
+        */
+        let peer = peerMap.get(fromUserId);
+        if (peer) {
+          await peer.setRemoteDescription(sdp);
+        }
+      }
+    })
+    // 交换candiate信息
+    client.on('candidate-msg', async (data) => {
+      let { fromUserId, toUserId, candidate } = data
+      if (userId === toUserId) {
+        // console.log(connectingUserIdList, fromUserId, "candidate");
+        /**
+          * @type {RTCPeerConnection}
+          */
+        let peer = peerMap.get(fromUserId);
+        if (peer) {
+          await peer.addIceCandidate(candidate)
+        }
+      }
+    })
+    client.on('client-leave', (data) => {
+      console.log(data);
+    })
+  });
 
-    // 停止录制时关闭 WebSocket
-    mediaRecorder.onstop = () => {
-      socketStore.socket.close();
-    };
-  } catch (error) {
-    console.error('Error starting screen capture:', error);
-    socketStore.socket.close();
+  stopBtn.value.addEventListener("click", () => {
+    isStopAudio = !isStopAudio;
+    isStopVideo = !isStopVideo;
+    for (let id of connectingUserIdList) {
+      let peer = peerMap.get(id);
+      if (peer) {
+        peer.getSenders().find(sender => sender.track.kind === 'audio').track.enabled = !isStopAudio;
+        peer.getSenders().find(sender => sender.track.kind === 'video').track.enabled = !isStopVideo;
+      }
+    }
+  })
 
-  }
-};
+  videoBtn.value.addEventListener("click", async () => {
+    let newStream = await getLocalMediaStream({
+      video: true,
+      audio: true
+    })
+    if (newStream) {
+      localStream = newStream;
+      setLocalVideoStream(offerVideo.value, localStream);
+      localStream.getVideoTracks().forEach(track => {
+        for (let id of connectingUserIdList) {
+          let peer = peerMap.get(id);
+          if (peer) {
+            peer.getSenders().find(sender => sender.track.kind === 'video').replaceTrack(track)
+          }
+        }
+      })
+    }
+  })
+  screenBtn.value.addEventListener("click", async () => {
+    let newStream = await getLocalScreenMediaStream({
+      video: {
+        cursor: 'always' | 'motion' | 'never',
+        displaySurface: 'application' | 'browser' | 'monitor' | 'window'
+      }
+    });
+    if (newStream) {
+      localStream = newStream;
+      setLocalVideoStream(offerVideo, localStream);
+      localStream.getVideoTracks().forEach(track => {
+        for (let id of connectingUserIdList) {
+          let peer = peerMap.get(id);
+          if (peer) {
+            peer.getSenders().find(sender => sender.track.kind === 'video').replaceTrack(track)
+          }
+        }
+      })
+    }
+  })
 
-const StopRecording = () => {
-  if (mediaRecorder) {
-    mediaRecorder.stop();
-  }
-  if (stream) {
-    stream.getTracks().forEach(track => track.stop());
-  }
-  videoRef.value.srcObject = null;
-};
+
+})
 
 </script>
 
 <template>
-  <div class="live-screen">
-    <video class="custom-video" autoplay controls ref="videoRef"></video>
-    <div>
-      <button @click="StartRecording">Start Recording</button>
-      <button disabled @click="StopRecording">Stop Recording</button>
+  <div style="width: 1000px; height: 1000px; background: goldenrod;">
+    <input ref="roomInput" id="roomId" type="text" value="会议房间1" placeholder="请输入房间号" />
+    <input ref="userInput" id="userId" type="text" value="123" placeholder="请输入用户昵称" />
+    <button ref="startBtn" class="startBtn">开始通话</button>
+    <button ref="stopBtn" class="stopBtn">暂停/恢复通话</button>
+    <button ref="videoBtn" class="videoBtn">视频通话</button>
+    <button ref="screenBtn" class="screenBtn">屏幕共享</button>
+    <div class="video-container">
+      <video ref="offerVideo" autoplay controls muted></video>
+      <!-- 确保这里使用 ref 而不是 id -->
     </div>
   </div>
 </template>
-
 <style scoped lang="scss">
-.live-screen {
+.video-container {
   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px dashed pink;
 }
 
-/* 隐藏时间显示 */
-.custom-video::-webkit-media-controls-current-time-display,
-.custom-video::-webkit-media-controls-time-remaining-display {
-  display: none;
-}
-
-/* 隐藏进度条 */
-.custom-video::-webkit-media-controls-timeline {
-  display: none;
-}
-
-/* Firefox 浏览器 */
-.custom-video::-moz-range-track {
-  display: none;
-}
-
-.custom-video {
-  width: 100%;
-  border: 2px solid #333;
-  background-color: #000;
-}
-
-button {
-  margin: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  border: none;
-  background-color: #007bff;
-  color: white;
-  border-radius: 5px;
-}
-
-button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
+.video-container video {
+  border: 1px dashed pink;
+  width: 35%;
+  height: 35%;
 }
 </style>
